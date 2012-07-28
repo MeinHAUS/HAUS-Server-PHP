0e18fc0ae03f833d12fa4c3767c0e0f4cb446c94
mhensler <mark.hensler@gmail.com>
2012-07-28 11:54:48 -0700

    replace hard coded page title with cfg[Name]
    add Name and Location to About

--- Servers/php/index.php
+++ Servers/php/index.php
@@ -75,7 +75,7 @@ function PrintDeviceMenu($menu, $depth=0) {
 <head>
 <meta charset="ISO-8859-1" />
 <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
-<title>HAUS</title>
+<title><?php echo $cfg["Name"]; ?></title>
 <link rel="icon" type="image/png" href="images/house.png" />
 <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.5.1/build/cssreset/cssreset-min.css" />
 <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.5.1/build/cssbase/cssbase-min.css" />
@@ -88,7 +88,7 @@ function PrintDeviceMenu($menu, $depth=0) {
 
 
 <div id="ControlPanel">
-	<p id="title">HAUS</p>
+	<p id="title"><?php echo $cfg["Name"]; ?></p>
 	<!-- ToggleConsole -->
 	<?php if ($cfg["Debug"]) { echo '<div class="ControlButton" id="ToggleDebug" title="Debug"></div>'; } ?>
 	<div class="ControlButton hide" id="ToggleSettings" title="Settings"></div>
@@ -117,10 +117,19 @@ PrintDeviceMenu($cfg['DeviceMenu'], 0);
 
 
 <div id="Help" class="hide">
-	<b>Author:</b> Mark Hensler<br />
-	<b>GitHub:</b> <a href="https://github.com/mhensler/HAUS" target="_blank">github.com/mhensler/HAUS</a><br />
+	<b><?php echo $cfg["Name"]; ?></b><br />
+<?php
+	if (is_array($cfg["Location"]) && !empty($cfg["Location"])) {
+		echo "\t<address>";
+		foreach ($cfg["Location"] as $line) {
+			echo "\t$line<br />\n";
+		}
+		echo "\t</address>\n";
+	}
+?>
 	<br />
-	<a href="http://www.famfamfam.com/lab/icons/silk/" target="_blank">Silk icon set</a> by <a href="mailto:mjames@gmail.com">Mark James</a><br />
+	<b>GitHub:</b> <a href="https://github.com/mhensler/HAUS" target="_blank">github.com/mhensler/HAUS</a><br />
+	<b>Icons:</b> <a href="http://www.famfamfam.com/lab/icons/silk/" target="_blank">Silk Icon Set</a> by <a href="mailto:mjames@gmail.com">Mark James</a><br />
 </div>
 
 </body>
